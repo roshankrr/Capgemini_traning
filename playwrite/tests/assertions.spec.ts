3fd2d613d16e42cc491fd8f135230bd65173245b
roshankrr <roshankrr16@gmail.com>
2026-03-09 15:57:49 +0530

    update

--- playwrite/tests/assertions.spec.ts
+++ playwrite/tests/assertions.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+test.use({ actionTimeout: 5000 });
 
 test("auto-assertions", async ({ page }) => {
   await page.goto("https://practicetestautomation.com/practice-test-login/");
@@ -9,15 +10,15 @@ test("auto-assertions", async ({ page }) => {
   await expect(uname).toHaveValue("roshan");
   await expect(uname).toHaveScreenshot();
 });
+
 test.only("non-assertions", async ({ page }) => {
   await page.goto("https://practicetestautomation.com/practice-test-login/");
   let uname = page.getByRole("textbox", { name: "username" });
   let pass = page.getByRole("textbox", { name: "password" });
 
   let label = await page.locator('//*[@id="form"]/div[1]/label');
-  await expect.soft(label).toHaveText("Username");
 
-  await uname.fill("roshan");
+  await uname.fill("roshan", { timeout: 5000 });
   await expect("roshan").toContain("ros");
   await expect([1, 23, 45, 4, 3, 13, 4]).toContain(23);
   await expect("roshan").not.toContain("rohs");
